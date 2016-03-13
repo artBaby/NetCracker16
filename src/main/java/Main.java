5a085fbc161ccf40fccf13e8ba6eea068482aa4a
artbaby <artemiy5555563@gmail.com>
2016-03-13 15:18:30 +0300

    gitTest!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("DreamTeam");
-        System.out.println("GitTest");
+        System.out.println("GitTest!");
 
 
     }
