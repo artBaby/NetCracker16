213fc0862b472e8548ba4a231441c15aa2f96aab
dasha2893 <missis.Bobiakowa@yandex.ru>
2016-03-16 15:49:21 +0300

    added log4j (output in console)

--- src/main/java/netCrackerTestApp/Main.java
+++ src/main/java/netCrackerTestApp/Main.java
@@ -1,14 +1,20 @@
 package netCrackerTestApp;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * Created by Artemiy on 13.03.2016.
  */
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("DreamTeam");
-        System.out.println("GitTest");
+    private static final Logger log = Logger.getLogger(Main.class);
 
+    public static void main(String[] args) {
+
+        log.info("DreamTeam");
+        log.info("GitTest");
+        log.error("Error!");
 
     }
 
