5373953da6fba97b40e8a1dcbd3ca6e7627118ea
dasha2893 <missis.Bobiakowa@yandex.ru>
2016-03-20 22:27:29 +0300

    added  sentiment analysis library

---
+++ src/main/java/netCrackerTestApp/SentimentAnalysis.java
@@ -0,0 +1,27 @@
+package netCrackerTestApp;
+
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.util.List;
+import java.util.Properties;
+
+public class SentimentAnalysis {
+    public static void main(String[] args) {
+        String text = "I am feeling very sad and frustrated.";
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        Annotation annotation = pipeline.process(text);
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+            System.out.println(sentiment + "\t" + sentence);
+        }
+    }
+}
